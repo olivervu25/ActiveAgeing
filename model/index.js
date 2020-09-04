b2e87e3579015c4c671182446d9db1a8d3da2257
nguyenchiemdu <nnguenchiemdu@gmail.com>
2020-09-04 16:18:56 +0700

    home page foundation commit

--- model/index.js
+++ model/index.js
@@ -2,5 +2,5 @@ const init = () => {
     console.log('Window loaded')
     view.setActiveScreen('welcomeScreen')
 }
-// window.onload = init
+// code like this not worked: window.onload = init // Du : component must be render before js file has run
 init();
