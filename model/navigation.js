6e10318693d2b3c1902048b8dadd37062c632947
longvt <longvu2000.hust@gmail.com>
2020-09-05 16:38:08 +0700

    Build the foundation of Scholarship page

--- model/navigation.js
+++ model/navigation.js
@@ -6,7 +6,18 @@
     });
     
     //  univesity page
+
+
+    $('#scholarship').on('click',function(){
+        view.setActiveScreen('scholarshipScreen')
+    });
+
     $('#university').on('click',function(){
         console.log('Change to university page');
         view.setActiveScreen('universityScreen');
     });
+
+    $('#about').on('click',function(){
+        view.setActiveScreen('welcomeScreen');
+        document.getElementById("about-section").scrollIntoView();
+    })
